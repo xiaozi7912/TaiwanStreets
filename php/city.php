70ad03eacc7ae05de3f8f0e2a590a0872d654400
Larry <xiaozi7912@gmail.com>
2015-01-14 11:40:46 +0800

    [Title] Modified API & add SQL file. [Symptom] [Root Cause] [Solution]

--- php/city.php
+++ php/city.php
@@ -2,28 +2,13 @@
 	require_once("config.php");
 	header("Content-Type:application/json");
 	$response = array();
-	$command = "SELECT CITY,GROUP_CONCAT(DISTINCT MAILCODE) AS MAILCODES,GROUP_CONCAT(DISTINCT COUNTRY) AS COUNTRIES FROM TAIWANSTREETS.TAIWANSTREETS GROUP BY CITY ORDER BY MAILCODE;";
+	$command = "SELECT CITY FROM TAIWANSTREETS.TAIWANSTREETS GROUP BY CITY ORDER BY MAILCODE;";
 
 	// $response["command"] = $command;
 	if($result = $conn->query($command)){
 		$rows = array();
-		while($row = $result->fetch_assoc()){
-			$formatted = array();
-			$rCodes = preg_split("/,/", $row["MAILCODES"]);
-			$rCountries = preg_split("/,/", $row["COUNTRIES"]);
-			// $row["ROADS"] = preg_split("/,/", $row["ROADS"]);
-
-			$formatted["CITY"] = $row["CITY"];
-			$formatted["COUNTRIES"] = array();
-
-			for($i=0;$i<count($rCodes);$i++){
-				$fCountries = array();
-				$fCountries["MAILCODE"] = $rCodes[$i];
-				$fCountries["COUNTRY"] = $rCountries[$i];
-				
-				array_push($formatted["COUNTRIES"], $fCountries);
-			}			
-			array_push($rows, $formatted);
+		while($row = $result->fetch_assoc()){			
+			array_push($rows, $row["CITY"]);
 		}
 		$response["result"] = $rows;
 		$result->close();
--- php/street.php
+++ php/street.php
@@ -1,15 +1,17 @@
 <?php
 	require_once("config.php");
 	header("Content-Type:application/json");
-	$mailcode = $_GET["mailcode"];
+	$city = $_GET["city"];
 	$response = array();
-	$command = sprintf("SELECT ROAD FROM TAIWANSTREETS.TAIWANSTREETS WHERE MAILCODE=%s;",$mailcode);
+	$command = sprintf("SELECT COUNTRY,MAILCODE,GROUP_CONCAT(DISTINCT ROAD) AS ROADS FROM TAIWANSTREETS.TAIWANSTREETS WHERE CITY='%s' GROUP BY COUNTRY ORDER BY MAILCODE;",$city);
 
 	// $response["command"] = $command;
+	$conn->query("SET SESSION GROUP_CONCAT_MAX_LEN = 1000000;");
 	if($result = $conn->query($command)){
 		$rows = array();
 		while($row = $result->fetch_assoc()){
-			array_push($rows, $row["ROAD"]);
+			$row["ROADS"] = preg_split("/,/", $row["ROADS"]);
+			array_push($rows, $row);
 		}
 		$response["result"] = $rows;
 		$result->close();
