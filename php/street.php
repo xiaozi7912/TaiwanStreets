85e1c4dc7b2215845ba16f59102dce5efffa4097
Larry <xiaozi7912@gmail.com>
2015-01-29 10:08:41 +0800

    [Title] Change SQL command. [Symptom] NA [Root Cause] NA [Solution] NA

--- php/street.php
+++ php/street.php
@@ -4,13 +4,12 @@
 	$city = $_GET["city"];
 	$country = $_GET["country"];
 	$response = array();
-	$command = sprintf("SELECT MAILCODE,GROUP_CONCAT(DISTINCT ROAD) AS ROADS FROM TAIWANSTREETS.TAIWANSTREETS WHERE CITY='%s' AND COUNTRY='%s' GROUP BY MAILCODE ORDER BY MAILCODE;",$city,$country);
+	$command = sprintf("SELECT MAILCODE,ROAD FROM TAIWANSTREETS.TAIWANSTREETS WHERE CITY='%s' AND COUNTRY='%s' ORDER BY MAILCODE;",$city,$country);
 
 	// $response["command"] = $command;
 	if($result = $conn->query($command)){
 		$rows = array();
 		while($row = $result->fetch_assoc()){
-			$row["ROADS"] = preg_split("/,/", $row["ROADS"]);
 			array_push($rows, $row);
 		}
 		$response["result"] = $rows;
